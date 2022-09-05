9388e063c452c24b6756f0b5d7a9b487a706a794
Mars4all <88582613+Mars4all@users.noreply.github.com>
2022-09-06 01:19:48 +0200

    update ist mega cool

--- main.js
+++ main.js
@@ -1,31 +1,44 @@
 $(document).ready(function() {
 	// when document loaded:
-    
+  // Die loik der Berechnung  
     $("#div16").hide();
+    $("#table30").hide();
+    $("#table60").hide();
+    $("#table1").hide();
+    $("#table2").hide();
    
+	$("#buttonberechnen").mouseup(function() {
+        let KWH = $("#kwh").val()*1;
 
-	$("#button1").mouseup(function() {
-        alert("button4 gedrückt");
+
+
+
+
+        $("#Nord30").html(KWH);
+        $("#West30").html(KWH);
+        $("#Ost30").html(KWH);
+        $("#Sued30").html(KWH);
+
+        $("#Nord60").html(KWH);
+        $("#West60").html(KWH);
+        $("#Ost60").html(KWH);
+        $("#Sued60").html(KWH)
+
+        
+        
+       // let Anzahl300module = Math.round(Tagesverbrauch / Modultagesleistung);  // Quelle: https://www.energie-experten.org/erneuerbare-energien/photovoltaik/planung/anlagen-groesse
+       
+        
+
+       $("#table30").show();
+       $("#table60").show();
+       $("#table1").show();
+       $("#table2").show();
+
 	});
 
-	$("#button2").mouseup(function() {
-        alert("button2 gedrückt");
-	});
-
-	$("#div5").mouseup(function() {
-        let KWH = $("#input17").val()*1;
-        let Module = Math.floor(KWH / 150);
-        let kosten = Module * 270;
-        let kostenkwh = 0.35;
-        let rechnet = Math.floor(kosten / (kostenkwh * KWH));
-        $("#div16").html("Sie benötigen ca. " + Module + " Module. <br> Ihre kosten belaufen sich auf "+ kosten+
-        " Euro Material <br> Das System wird sich nach "+ rechnet+ " Jahren rechnen");
-        $("#div16").show(2500);
-	});
-
-	$("#button3").mouseup(function() {
-        alert("button3 gedrückt");
-	});
+	
+// Hier Login Logik
 
 	$("#Einloggen").mouseup(function() {
                 let mail = $("#Email").val();
