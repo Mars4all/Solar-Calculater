39c9565e5f17e2de2e840a3f8c2755e30071f334
Mars4all <88582613+Mars4all@users.noreply.github.com>
2022-10-03 01:28:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-
 $(document).ready(function() {
     let modul_400w = 400;
     let kosten_modul_400w = 249; //Quelle Ebay.de
+    let kostengesamt_in_Euro = (modul_400w * 2,5)*1500;
   
     $("#table0").hide();
     $("#table30").hide();
@@ -11,37 +11,44 @@ $(document).ready(function() {
     $("#table2").hide();
     $("#table3").hide();
     $("#table4").hide();
+    $("#zurueck_zu_den_Anbietern").hide();
+    $("#Text_verbergen_vor_Login_zu_Anbieter").hide();
     $("#button_Daten_uebernehmen2").hide();
    
 	$("#buttonberechnen").mouseup(function() {
         let KWH = $("#kwh").val()*1;
         let kostenkwh = $("#kwh-Preis").val()*1;
-
         let Tagesverbrauch = KWH / 360;
         let kostenjahr = kostenkwh * KWH;
+        let min_Tagesleistung = modul_400w*4;
+       
+
+       
+
 
         // Berechnung mit einer Neigung von 60%
-        let Anzahl_400w_module_0_Grad = Math.round(Tagesverbrauch / ((4*modul_400w)/ 1000)/0.87);
+        let Anzahl_400w_module_0_Grad = Math.round((Tagesverbrauch /((min_Tagesleistung)/ 1000) *0.87));
+        
 
         // Berechnung mit einer Neigung von 30%
-        let Anzahl_400w_module_nord_30 = Math.round(Tagesverbrauch / ((4*modul_400w)/ 1000)/0.61);
-        let Anzahl_400w_module_west_30 = Math.round(Tagesverbrauch / ((4*modul_400w)/ 1000)/0.82);
-        let Anzahl_400w_module_ost_30 = Math.round(Tagesverbrauch / ((4*modul_400w)/ 1000)/0.82);
-        let Anzahl_400w_module_sued_30 = Math.round(Tagesverbrauch / ((4*modul_400w)/ 1000));
+        let Anzahl_400w_module_nord_30 = Math.round(Tagesverbrauch / ((min_Tagesleistung)/ 1000)/0.61);
+        let Anzahl_400w_module_west_30 = Math.round(Tagesverbrauch / ((min_Tagesleistung)/ 1000)/0.82);
+        let Anzahl_400w_module_ost_30 = Math.round(Tagesverbrauch / ((min_Tagesleistung)/ 1000)/0.82);
+        let Anzahl_400w_module_sued_30 = Math.round(Tagesverbrauch / ((min_Tagesleistung)/ 1000));
 
 
          // Berechnung mit einer Neigung von 45%
-         let Anzahl_400w_module_nord_45 = Math.round(Tagesverbrauch / ((4*modul_400w)/ 1000)/0.475);
-         let Anzahl_400w_module_west_45 = Math.round(Tagesverbrauch / ((4*modul_400w)/ 1000)/0.77);
-         let Anzahl_400w_module_ost_45 = Math.round(Tagesverbrauch / ((4*modul_400w)/ 1000)/0.77);
-         let Anzahl_400w_module_sued_45 = Math.round(Tagesverbrauch / ((4*modul_400w)/ 1000)/0.99);
+         let Anzahl_400w_module_nord_45 = Math.round(Tagesverbrauch / ((min_Tagesleistung)/ 1000)/0.475);
+         let Anzahl_400w_module_west_45 = Math.round(Tagesverbrauch / ((min_Tagesleistung)/ 1000)/0.77);
+         let Anzahl_400w_module_ost_45 = Math.round(Tagesverbrauch / ((min_Tagesleistung)/ 1000)/0.77);
+         let Anzahl_400w_module_sued_45 = Math.round(Tagesverbrauch / ((min_Tagesleistung)/ 1000)/0.99);
 
         // Berechnung mit einer Neigung von 60%
 
-        let Anzahl_400w_module_nord_60 = Math.round(Tagesverbrauch / ((4*modul_400w)/ 1000)/0.35);
-        let Anzahl_400w_module_west_60 = Math.round(Tagesverbrauch / ((4*modul_400w)/ 1000)/0.70);
-        let Anzahl_400w_module_ost_60 = Math.round(Tagesverbrauch / ((4*modul_400w)/ 1000)/0.70);
-        let Anzahl_400w_module_sued_60 = Math.round(Tagesverbrauch / ((4*modul_400w)/ 1000)/0.94);
+        let Anzahl_400w_module_nord_60 = Math.round(Tagesverbrauch / ((min_Tagesleistung)/ 1000)/0.35);
+        let Anzahl_400w_module_west_60 = Math.round(Tagesverbrauch / ((min_Tagesleistung)/ 1000)/0.70);
+        let Anzahl_400w_module_ost_60 = Math.round(Tagesverbrauch / ((min_Tagesleistung)/ 1000)/0.70);
+        let Anzahl_400w_module_sued_60 = Math.round(Tagesverbrauch / ((min_Tagesleistung)/ 1000)/0.94);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -52,10 +59,14 @@ $(document).ready(function() {
         $("#Ost_0").html(Anzahl_400w_module_0_Grad);
         $("#Sued_0").html(Anzahl_400w_module_0_Grad);
 
-        let kostenNord0 = Anzahl_400w_module_0_Grad*kosten_modul_400w;
-        let kostenWest0 = Anzahl_400w_module_0_Grad*kosten_modul_400w;
-        let kostenOst0 = Anzahl_400w_module_0_Grad*kosten_modul_400w;
-        let kostenSued0 = Anzahl_400w_module_0_Grad*kosten_modul_400w;
+        let kostenNord0 = Math.floor((Anzahl_400w_module_0_Grad/2.5)*1500);
+        let kostenWest0 = Math.floor((Anzahl_400w_module_0_Grad/2.5)*1500);
+        let kostenOst0 = Math.floor((Anzahl_400w_module_0_Grad/2.5)*1500);
+        let kostenSued0 = Math.floor((Anzahl_400w_module_0_Grad/2.5)*1500);
+
+/*
+
+    Rechnung für die Amortisierung
 
         let JahrenNord0 = Math.round( (kostenNord0 /  kostenjahr)* 12 );
                 let MonatNord0 = JahrenNord0%12;
@@ -69,13 +80,13 @@ $(document).ready(function() {
         let Jahrensued0 = Math.round(  (kostenSued0 /kostenjahr) * 12 );
                 let MonatSued0 = Jahrensued0%12;
                 let JahreSued0 = Math.round(Jahrensued0/12);
-
+*/
 
         // Zuweisung Kosten bei Flachdach
-        $("#kosten0Nord").html(kostenNord0+",- € <br> Rechnet sich nach ca "+JahreNord0+" Jahren und "+ MonatNord0+" Monate");
-        $("#kosten0West").html(kostenWest0+",- € <br> Rechnet sich nach ca "+JahreWest0+" Jahren und " +MonatWest0 + " Monate");
-        $("#kosten0Ost").html(kostenOst0+",- € <br> Rechnet sich nach ca "+JahreOst0+" Jahren und "+MonatOdt0+ " Monate");
-        $("#kosten0Sued").html(kostenSued0+",- € <br> Rechnet sich nach ca "+JahreSued0+" Jahren und " + MonatSued0+ " Monate");
+        $("#kosten0Nord").html(kostenNord0+",- € ");
+        $("#kosten0West").html(kostenWest0+",- € ");
+        $("#kosten0Ost").html(kostenOst0+",- € ");
+        $("#kosten0Sued").html(kostenSued0+",- € ");
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // Zuweisung bei 30° Schräge
@@ -84,10 +95,14 @@ $(document).ready(function() {
         $("#Ost30").html(Anzahl_400w_module_ost_30);
         $("#Sued30").html(Anzahl_400w_module_sued_30);
 
-        let kostenNord30 = Anzahl_400w_module_nord_30*kosten_modul_400w;
-        let kostenWest30 = Anzahl_400w_module_west_30*kosten_modul_400w;
-        let kostenOst30 = Anzahl_400w_module_ost_30*kosten_modul_400w;
-        let kostenSued30 = Anzahl_400w_module_sued_30*kosten_modul_400w;
+        let kostenNord30 = Math.floor(( Anzahl_400w_module_nord_30/2.5)*1500);
+        let kostenWest30 = Math.floor((Anzahl_400w_module_west_30 /2.5)*1500);
+        let kostenOst30 = Math.floor(( Anzahl_400w_module_ost_30/2.5)*1500);
+        let kostenSued30 = Math.floor((Anzahl_400w_module_sued_30 /2.5)*1500);
+
+/*
+
+    Rechnung für die Amortisierung
 
         let JahrenNord30 = Math.round( (kostenNord30 /  kostenjahr)*12 );
                 let MonatNord30 = JahrenNord30%12;
@@ -102,12 +117,12 @@ $(document).ready(function() {
                 let MonatSued30 = Jahrensued30%12;
                 let JahreSued30 = Math.floor(Jahrensued30/12);
 
-
+*/
          // Zuweisung Kosten 30°
-        $("#kosten30Nord").html(kostenNord30+",- € <br> Rechnet sich nach ca "+JahreNord30+ " Jahren und "+MonatNord30+" Monate");
-        $("#kosten30West").html(kostenWest30+",- € <br> Rechnet sich nach ca "+JahreWest30+" Jahen und " + MonatWest30+ " Monate");
-        $("#kosten30Ost").html(kostenOst30+",- € <br> Rechnet sich nach ca "+JahreOst30+" Jahren und "+MonatOst30+" Monate");
-        $("#kosten30Sued").html(kostenSued30+",- € <br> Rechnet sich nach ca "+JahreSued30+" Jahren und "+ MonatSued30+ " Monate");
+        $("#kosten30Nord").html(kostenNord30+",- € ");
+        $("#kosten30West").html(kostenWest30+",- € ");
+        $("#kosten30Ost").html(kostenOst30+",- € ");
+        $("#kosten30Sued").html(kostenSued30+",- € ");
  
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
          // Zuweisung bei 45° Schräge
@@ -116,11 +131,15 @@ $(document).ready(function() {
          $("#Ost45").html(Anzahl_400w_module_ost_45);
          $("#Sued45").html(Anzahl_400w_module_sued_45);
 
-         let kostenNord45 = Anzahl_400w_module_nord_45*kosten_modul_400w;
-         let kostenWest45 = Anzahl_400w_module_west_45*kosten_modul_400w;
-         let kostenOst45 = Anzahl_400w_module_ost_45*kosten_modul_400w;
-         let kostenSued45 = Anzahl_400w_module_sued_45*kosten_modul_400w;
+         let kostenNord45 = Math.floor((Anzahl_400w_module_nord_45 /2.5)*1500);
+         let kostenWest45 = Math.floor((Anzahl_400w_module_west_45 /2.5)*1500);
+         let kostenOst45 = Math.floor((Anzahl_400w_module_ost_45 /2.5)*1500);
+         let kostenSued45 = Math.floor((Anzahl_400w_module_sued_45 /2.5)*1500);
 
+
+ /*
+
+    Rechnung für die Amortisierung        
          let JahrenNord45 = Math.round( (kostenNord45 /  kostenjahr)*12 );
                 let MonatNord45 = JahrenNord45%12;
                 let JahreNord45 = Math.floor(JahrenNord45/12);
@@ -133,12 +152,12 @@ $(document).ready(function() {
          let Jahrensued45 = Math.round(  (kostenSued45 /kostenjahr)*12 );
                 let Monatsued45 = Jahrensued45%12;
                 let Jahresued45 = Math.floor(Jahrensued45/12);
-
+*/
          // Zuweisung Kosten 45° 
-         $("#kosten45Nord").html(kostenNord45+",- € <br> Rechnet sich nach ca "+JahreNord45+" Jahren und "+MonatNord45+" Monate");
-         $("#kosten45West").html(kostenWest45+",- € <br> Rechnet sich nach ca "+Jahrewest45+" Jahren und "+Monatwest45+ " Monate");
-         $("#kosten45Ost").html(kostenOst45+",- € <br> Rechnet sich nach ca "+Jahreost45+" Jahren und " +Monatost45+ " Monate");
-         $("#kosten45Sued").html(kostenSued45+",- € <br> Rechnet sich nach ca "+Jahresued45+" Jahren und "+Monatsued45 + " Monate");
+         $("#kosten45Nord").html(kostenNord45+",- € ");
+         $("#kosten45West").html(kostenWest45+",- € ");
+         $("#kosten45Ost").html(kostenOst45+",- € ");
+         $("#kosten45Sued").html(kostenSued45+",- € ");
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // Zuweisung bei 60° Schräge
@@ -147,11 +166,13 @@ $(document).ready(function() {
         $("#Ost60").html(Anzahl_400w_module_ost_60);
         $("#Sued60").html(Anzahl_400w_module_sued_60);
 
-        let kostenNord60 = Anzahl_400w_module_nord_60*kosten_modul_400w;
-         let kostenWest60 = Anzahl_400w_module_west_60*kosten_modul_400w;
-         let kostenOst60 = Anzahl_400w_module_ost_60*kosten_modul_400w;
-         let kostenSued60 = Anzahl_400w_module_sued_60*kosten_modul_400w;
+        let kostenNord60 = Math.floor((Anzahl_400w_module_nord_60/2.5)*1500);
+         let kostenWest60 = Math.floor((Anzahl_400w_module_west_60/2.5)*1500);
+         let kostenOst60 = Math.floor((Anzahl_400w_module_ost_60/2.5)*1500);
+         let kostenSued60 = Math.floor((Anzahl_400w_module_sued_60/2.5)*1500);
+/*
 
+    Rechnung für die Amortisierung
          let JahrenNord60 = Math.round( (kostenNord60 /  kostenjahr)*12 );
                 let MonatNord60 = JahrenNord60%12;
                 let JahreNord60 = Math.floor(JahrenNord60/12);
@@ -164,13 +185,13 @@ $(document).ready(function() {
          let Jahrensued60 = Math.round(  (kostenSued60 /kostenjahr)*12 );
                 let Monatsued60 = Jahrensued60%12;
                 let Jahresued60 = Math.floor(Jahrensued60/12);
-
+*/
 
          // Zuweisung bei 60° Schräge
-         $("#kosten60Nord").html(kostenNord60+",- € <br> Rechnet sich nach ca "+JahreNord60+" Jahren und "+MonatNord60+ " Monate");
-         $("#kosten60West").html(kostenWest60+",- € <br> Rechnet sich nach ca "+Jahrewest60+" Jahren und "+ Monatwest60+ " Monate");
-         $("#kosten60Ost").html(kostenOst60+",- € <br> Rechnet sich nach ca "+Jahreost60+" Jahren und "+Monatost60+" Monate");
-         $("#kosten60Sued").html(kostenSued60+",- € <br> Rechnet sich nach ca "+Jahresued60+" Jahren und "+Monatsued60+" Monate");
+         $("#kosten60Nord").html(kostenNord60+",- € ");
+         $("#kosten60West").html(kostenWest60+",- € ");
+         $("#kosten60Ost").html(kostenOst60+",- € ");
+         $("#kosten60Sued").html(kostenSued60+",- € ");
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////      
 
@@ -207,12 +228,17 @@ $(document).ready(function() {
  });
 
  $(".versenden_daten_Anbieter").mouseup(function() {
-        alert("Ihre Daten werden zum Anbieter versendet, der Anbieter wird sich schnellstmöglichst bei Ihnen melden.");
+       
+        $(".versenden_daten_Anbieter").hide();
+        $("#formLogin").hide();
+        alert("Ihre Daten werden zum Anbieter versendet, sobald Sie sich angemeldet haben." );
+        $("#zurueck_zu_den_Anbietern").show();
+        $("#Text_verbergen_vor_Login_zu_Anbieter").show();
 
  });
 
  $(".zum_Anbieter_ohne_Anmeldung").mouseup(function() {
-       alert("Jetzt werden Sie zum Anbieter weitergeleitet.");
+       alert("Jetzt werden Sie zum Anbieter weitergeleitet");
 
 });
 
